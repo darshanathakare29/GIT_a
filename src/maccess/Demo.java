7f5d8409b4d1dce1928e47182eab42fcb8294f6b
darshanathakare29 <darshana2001thakre@gmail.com>
2025-07-01 17:02:05 +0530

    committed

---
+++ src/maccess/Demo.java
@@ -0,0 +1,24 @@
+package maccess;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int r=1;r<=3;r++) {
+	for(int c=1;c<=r;c++) {
+		System.out.print("* ");
+	}
+	
+	System.out.println();
+}
+
+for(int r=1;r<=2;r++) {
+	for(int c1=2;c1>=r;c1--) {
+		System.out.print("* ");
+	}
+	System.out.println();
+}
+	}
+	
+
+}
